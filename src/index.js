014de68ce6757c22a967bee482a6b8410620ef0a
JessCP25 <jpayanoc25@gmail.com>
2025-06-21 18:39:01 -0500

    Feat: Uso de High-Order Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App(props){
+  return(
+    <h2>¡{props.saludo}, {props.nombre}!</h2>
+  )
+}
+
+function withSaludo(WrapperComponent){
+  return function WrappedComponentWithSaludo(saludo){
+    return function ComponenteDeVerdad(props){
+      return (
+        <>
+          <WrapperComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </>
+      )
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo(App)('Hey');
+
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo nombre="Jessica" />,
   document.getElementById('root')
 );
