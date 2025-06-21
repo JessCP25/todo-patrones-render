401b6002ef690fd85487030bad7e4073b1b0876c
JessCP25 <jpayanoc25@gmail.com>
2025-06-21 00:15:38 -0500

    Feat: Uso de render props

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './TodoList.css'
+import React from "react";
+import "./TodoList.css";
 
 function TodoList(props) {
   return (
     <section>
       <ul>
-        {props.children}
+        {props.error && props.onError()}
+        {props.loading && props.onLoading()}
+        {!props.loading && !props.searchedTodos.length && props.onEmptyTodos()}
+        {props.searchedTodos.map((todo) => props.render(todo))}
       </ul>
     </section>
   );
